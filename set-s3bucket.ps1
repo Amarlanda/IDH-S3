67d33faa226175c84f50c5a5bcb01f7064a24fc6
Amar Landa <al@amarlanda.com>
2018-02-16 12:28:51 +0000

    first commit

---
+++ set-s3bucket.ps1
@@ -0,0 +1,11 @@
+﻿
+Write-S3Object -BucketName my-bucket -File file.txt -Key subfolder/remote-file.txt -CannedACLName Private -AccessKey accesskey -SecretKey secretKey
+
+$bucket = Get-S3Bucket -BucketName pizza-luvrs-aj
+
+$results = Get-ChildItem .\path\to\files -Recurse -Include "*.pdf" 
+foreach ($path in $results) {
+	Write-Host $path
+	$filename = [System.IO.Path]::GetFileName($path)
+	Write-S3Object -BucketName my-bucket -File $path -Key subfolder/$filename -CannedACLName Private -AccessKey accessKey -SecretKey secretKey
+}
