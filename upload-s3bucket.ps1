a9553b062dd6f6860280b103196e5f285fd12f71
amarlanda.com <al@amarlanda.com>
2018-04-09 16:21:07 +0100

    automated push 04/09/2018 16:21:07

---
+++ upload-s3bucket.ps1
@@ -0,0 +1,80 @@
+﻿function RecurseFolders([string]$path) {
+
+  $fc = New-Object -com Scripting.FileSystemObject
+  $folder = $fc.GetFolder($path)
+
+  # Iterate through subfolders
+  foreach ($i in $folder.SubFolders) {
+    $thisFolder = $i.Path
+
+    # Transform the local directory path to notation compatible with S3 Buckets and Folders
+    # 1. Trim off the drive letter and colon from the start of the Path
+    $s3Path = $thisFolder.ToString()
+    $s3Path = $s3Path.SubString(2)
+    # 2. Replace back-slashes with forward-slashes
+    # Escape the back-slash special character with a back-slash so that it reads it literally, like so: "\\"
+    $s3Path = $s3Path -replace "\\", "/"
+
+    # Upload directory to S3
+    Write-S3Object -BucketName Backups -Folder $thisFolder -KeyPrefix $s3Path
+  }
+
+  # If subfolders exist in the current folder, then iterate through them too
+  foreach ($i in $folder.subfolders) {
+    RecurseFolders($i.path)
+  }
+}
+6.D. Full PowerShell script
+The following is a full PowerShell script that will backup/upload a directory (including all subdirectories) from your local computer to an Amazon S3 Bucket.
+
+# Constants
+$sourceDrive = "C:\"
+$sourceFolder = "ImportantFiles"
+$sourcePath = $sourceDrive + $sourceFolder
+$s3Bucket = "Backups"
+$s3Folder = "Archive"
+
+# Constants – Amazon S3 Credentials
+$accessKeyID="EXAMPLEDXLAW52MZCGIA"
+$secretAccessKey="examplekfLK2c8NCFjlhhjxvYBxJwPkli1HosK4F"
+
+# Constants – Amazon S3 Configuration
+$config=New-Object Amazon.S3.AmazonS3Config
+$config.RegionEndpoint=[Amazon.RegionEndpoint]::"us-west-2"
+$config.ServiceURL = "https://s3-us-west-2.amazonaws.com/"
+
+# Instantiate the AmazonS3Client object
+$client=[Amazon.AWSClientFactory]::CreateAmazonS3Client($accessKeyID,$secretAccessKey,$config)
+
+# FUNCTION – Iterate through subfolders and upload files to S3
+function RecurseFolders([string]$path) {
+  $fc = New-Object -com Scripting.FileSystemObject
+  $folder = $fc.GetFolder($path)
+  foreach ($i in $folder.SubFolders) {
+    $thisFolder = $i.Path
+
+    # Transform the local directory path to notation compatible with S3 Buckets and Folders
+    # 1. Trim off the drive letter and colon from the start of the Path
+    $s3Path = $thisFolder.ToString()
+    $s3Path = $s3Path.SubString(2)
+    # 2. Replace back-slashes with forward-slashes
+    # Escape the back-slash special character with a back-slash so that it reads it literally, like so: "\\"
+    $s3Path = $s3Path -replace "\\", "/"
+    $s3Path = "/" + $s3Folder + $s3Path
+
+    # Upload directory to S3
+    Write-S3Object -BucketName $s3Bucket -Folder $thisFolder -KeyPrefix $s3Path
+  }
+
+  # If subfolders exist in the current folder, then iterate through them too
+  foreach ($i in $folder.subfolders) {
+    RecurseFolders($i.path)
+  }
+}
+
+# Upload root directory files to S3
+$s3Path = "/" + $s3Folder + "/" + $sourceFolder
+Write-S3Object -BucketName $s3Bucket -Folder $sourcePath -KeyPrefix $s3Path
+
+# Upload subdirectories to S3
+RecurseFolders($sourcePath)
